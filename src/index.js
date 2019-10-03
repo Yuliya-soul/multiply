607a63f7cfb04a1b896937a90010a49f50a9b0f4
Your Name <you@example.com>
2019-10-03 15:35:09 +0300

    update file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function multiply(first, second) {
   // your solution
+ 
+    var a = first.split('').reverse();
+    var b = second.split('').reverse();
+  
+    var c = [];
+  
+    for (var i = 0; i < a.length; i++) {
+      for (var j = 0; j < b.length; j++) {
+        var multipl = a[i] * b[j];
+        c[i + j] = (c[i + j]) ? c[i + j] + multipl : multipl;
+      }
+    }
+  
+    for (var i = 0; i < c.length; i++) {
+      var num = c[i] % 10;
+      var step = Math.floor(c[i] / 10);
+      c[i] = num;
+  
+      if (c[i + 1])
+        c[i + 1] += step;
+      else if (step!= 0)
+        c[i + 1] =step;
+    }
+  
+  
+    return c.reverse().join('');
+  
+
+
 }
+
+
